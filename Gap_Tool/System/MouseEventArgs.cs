578c9acd44ccec82edad19d4eec8d274a210bece
Iron-Hawk <48659134+Iron-Hawk@users.noreply.github.com>
2020-08-20 17:37:10 -0400

    Add files via upload

---
+++ Gap_Tool/System/MouseEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace System
+{
+    internal class MouseEventArgs
+    {
+        private Action<object, Windows.Forms.MouseEventArgs> gap_MouseDoubleClick;
+
+        public MouseEventArgs(Action<object, Windows.Forms.MouseEventArgs> gap_MouseDoubleClick)
+        {
+            this.gap_MouseDoubleClick = gap_MouseDoubleClick;
+        }
+    }
+}
